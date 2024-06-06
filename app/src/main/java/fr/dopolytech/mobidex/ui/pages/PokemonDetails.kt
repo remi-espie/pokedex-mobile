ae8b5bd8d4c1558a7200cbcfcd737da4e2e3935f
ESPIE <remi.espie@etu.umontpellier.fr>
2024-06-06 18:06:48 +0200

    feat: change sprite depending on the pitch & roll

--- app/src/main/java/fr/dopolytech/mobidex/ui/pages/PokemonDetails.kt
+++ app/src/main/java/fr/dopolytech/mobidex/ui/pages/PokemonDetails.kt
@@ -9,12 +9,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import fr.dopolytech.mobidex.ui.MyViewModel
+import kotlin.math.abs
 
 @Composable
 fun PokemonDetails(
@@ -26,6 +29,16 @@ fun PokemonDetails(
   val pokemon = viewModel.pokemon.collectAsState().value
 
   val sensor = viewModel.sensor.collectAsState().value
+  val initPitch = remember { sensor.pitch }
+  val initRoll = remember { sensor.roll }
+
+  val sprite = remember { mutableStateOf(pokemon?.sprites?.frontDefault) }
+
+  if (abs(initPitch - sensor.pitch) > 25 || abs(initRoll - sensor.roll) > 25) {
+    sprite.value = pokemon?.sprites?.backDefault
+  } else {
+    sprite.value = pokemon?.sprites?.frontDefault
+  }
 
   Column {
     if (pokemon == null) {
@@ -37,13 +50,11 @@ fun PokemonDetails(
     } else {
       Text(text = "Pokemon:${pokemon.name}")
       AsyncImage(
-          model = pokemon.sprites.frontDefault,
+          model = sprite.value,
           contentDescription = "${pokemon.name} default sprite",
           contentScale = ContentScale.Fit,
           filterQuality = FilterQuality.High,
-          modifier = Modifier
-              .width(200.dp)
-              .height(200.dp),
+          modifier = Modifier.width(200.dp).height(200.dp),
       )
     }
   }
