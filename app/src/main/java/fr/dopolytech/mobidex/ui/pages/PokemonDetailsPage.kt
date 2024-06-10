3b600385e7ac95b9e116de44a3f5a210babcddd4
BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>
2024-06-10 22:43:33 +0200

    feat: loading progress indicator now centered
    Signed-off-by: BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>

--- app/src/main/java/fr/dopolytech/mobidex/ui/pages/PokemonDetailsPage.kt
+++ app/src/main/java/fr/dopolytech/mobidex/ui/pages/PokemonDetailsPage.kt
@@ -1,15 +1,21 @@
 package fr.dopolytech.mobidex.ui.pages
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.unit.dp
 import fr.dopolytech.mobidex.ui.MyViewModel
 import kotlin.math.abs
@@ -20,30 +26,34 @@ fun PokemonDetailsPage(
     viewModel: MyViewModel,
     pokemonId: String,
 ) {
-  val pokemonIdInt = pokemonId.toInt()
-  LaunchedEffect(pokemonIdInt) { viewModel.fetchPokemon(pokemonIdInt) }
-  val pokemon = viewModel.pokemon.collectAsState().value
+    val pokemonIdInt = pokemonId.toInt()
+    LaunchedEffect(pokemonIdInt) { viewModel.fetchPokemon(pokemonIdInt) }
+    val pokemon = viewModel.pokemon.collectAsState().value
 
-  val sensor = viewModel.sensor.collectAsState().value
-  val initAzimuth = remember { sensor.azimuth }
+    val sensor = viewModel.sensor.collectAsState().value
+    val initAzimuth = remember { sensor.azimuth }
 
-  val sprite = remember { mutableStateOf(pokemon?.sprites?.frontDefault) }
+    val sprite = remember { mutableStateOf(pokemon?.sprites?.frontDefault) }
 
-  if (abs(initAzimuth - sensor.azimuth) > 22.5) {
-    sprite.value = pokemon?.sprites?.backDefault
-  } else {
-    sprite.value = pokemon?.sprites?.frontDefault
-  }
-
-  Column {
-    if (pokemon == null) {
-      CircularProgressIndicator(
-          modifier = Modifier.width(64.dp),
-          color = MaterialTheme.colorScheme.secondary,
-          trackColor = MaterialTheme.colorScheme.surfaceVariant,
-      )
+    if (abs(initAzimuth - sensor.azimuth) > 22.5) {
+        sprite.value = pokemon?.sprites?.backDefault
     } else {
-      PokemonDetails(pokemon = pokemon)
+        sprite.value = pokemon?.sprites?.frontDefault
     }
-  }
+
+    Column {
+        if (pokemon == null) {
+            Column(verticalArrangement = Arrangement.Center, modifier = Modifier.fillMaxSize()) {
+                Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxSize()) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.width(64.dp),
+                        color = MaterialTheme.colorScheme.secondary,
+                        trackColor = MaterialTheme.colorScheme.surfaceVariant,
+                    )
+                }
+            }
+        } else {
+            PokemonDetails(pokemon = pokemon)
+        }
+    }
 }
