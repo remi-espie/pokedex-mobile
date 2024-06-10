f1a16ad756d4d99be8a597d1e5e0438ac5f4414e
BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>
2024-06-10 18:45:43 +0200

    feat: customise details upper card
    Signed-off-by: BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>

--- app/src/main/java/fr/dopolytech/mobidex/ui/components/PokemonDetails.kt
+++ app/src/main/java/fr/dopolytech/mobidex/ui/components/PokemonDetails.kt
@@ -1,16 +1,23 @@
 package fr.dopolytech.mobidex.ui.components
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.capitalize
@@ -23,22 +30,77 @@ import fr.dopolytech.mobidex.type.Pokemon
 @Composable
 fun PokemonDetails(pokemon: Pokemon) {
     Surface(modifier = Modifier.fillMaxSize()) {
-        Card(modifier = Modifier.padding(8.dp)) {
-            Row {
-                AsyncImage(
-                    model = pokemon.sprites.frontDefault,
-                    contentDescription = "${pokemon.name} default sprite",
-                    contentScale = ContentScale.Fit,
-                    filterQuality = FilterQuality.High,
-                    modifier = Modifier
-                        .width(200.dp)
-                        .height(200.dp),
-                )
-                Column(modifier = Modifier.padding(0.dp, 20.dp)) {
-                    Text(text = pokemon.name.capitalize(Locale.current), fontSize = 24.sp)
+        Column(verticalArrangement = Arrangement.spacedBy(15.dp)) {
+            DetailsCard(pokemon = pokemon)
+        }
+    }
+}
+
+@Composable
+private fun DetailsCard(pokemon: Pokemon) {
+    Card(modifier = Modifier.padding(25.dp)) {
+        Row {
+            // Picture
+            AsyncImage(
+                model = pokemon.sprites.frontDefault,
+                contentDescription = "${pokemon.name} default sprite",
+                contentScale = ContentScale.Fit,
+                filterQuality = FilterQuality.High,
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(200.dp),
+            )
+            Column(modifier = Modifier.padding(0.dp, 20.dp)) {
+                // Name and ID
+                Text(text = pokemon.name.capitalize(Locale.current), fontSize = 24.sp)
+                Text(text = "ID ${pokemon.id}")
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                // Types
+                Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                    for (type in pokemon.types.listIterator()) {
+                        val colours = determineTypeColour(type.type.name)
+                        Text(
+                            text = type.type.name.replace("-", " "),
+                            modifier = Modifier
+                                .background(
+                                    color = Color(colours[0]),
+                                    shape = RoundedCornerShape(20.dp)
+                                )
+                                .border(
+                                    border = BorderStroke(2.dp, Color(colours[1])),
+                                    shape = RoundedCornerShape(20.dp)
+                                )
+                                .padding(8.dp, 4.dp)
+                        )
+                    }
                 }
             }
         }
     }
+}
 
+fun determineTypeColour(name: String): List<Long> {
+    return when (name) {
+        "bug" -> listOf(0xff389a52, 0xff184e23)
+        "dark" -> listOf(0xff5a5979, 0xff040706)
+        "dragon" -> listOf(0xff5fcdd6, 0xff458a95)
+        "electric" -> listOf(0xfffefb75, 0xffe4e430)
+        "fairy" -> listOf(0xffe81469, 0xff9a1748)
+        "fighting" -> listOf(0xffef6138, 0xff963f24)
+        "fire" -> listOf(0xfffd4c5a, 0xffaa2222)
+        "flying" -> listOf(0xff94b3c9, 0xff4a677d)
+        "ghost" -> listOf(0xff95658e, 0xff323266)
+        "grass" -> listOf(0xff27cb4d, 0xff147b3f)
+        "ground" -> listOf(0xff6f481f, 0xffa87331)
+        "ice" -> listOf(0xffd8f0fa, 0xff7ecdee)
+        "normal" -> listOf(0xffca98a7, 0xff734f59)
+        "poison" -> listOf(0xff9a68d8, 0xff5d2b8b)
+        "psychic" -> listOf(0xfff61d91, 0xffa5286d)
+        "rock" -> listOf(0xff893e25, 0xff4c160d)
+        "steel" -> listOf(0xff41be94, 0xff517a6c)
+        "water" -> listOf(0xff86a8fc, 0xff1851e0)
+        else -> listOf(0xffffffff, 0xffffffff)
+    }
 }
